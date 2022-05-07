b5db90c04ab571e4093b8103c3e9f6131a4a83d6
Erlon Charles <erloncharlesbf@gmail.com>
2022-05-06 21:15:42 -0300

    Ajustes no Menu

--- wp-content/themes/bsd/header.php
+++ wp-content/themes/bsd/header.php
@@ -35,7 +35,8 @@
 
             <nav id="site-navigation" class="main-navigation">
                 <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
-                    <?php esc_html_e( 'Categorias', 'bsd' ); ?>
+                    <span>Categorias</span>
+                    <i class="dashicons-menu"></i>
                 </button>
                 <?php wp_nav_menu( [ 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu', ] ); ?>
             </nav><!-- #site-navigation -->
