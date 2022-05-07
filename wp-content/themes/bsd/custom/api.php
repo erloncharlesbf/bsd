1cba6d536c29a252b7feef3efbb37a753bf0a7fc
Erlon Charles <erloncharlesbf@gmail.com>
2022-05-07 14:34:21 -0300

    Listagem de slideshows

--- wp-content/themes/bsd/custom/api.php
+++ wp-content/themes/bsd/custom/api.php
@@ -10,6 +10,10 @@ add_action( 'rest_api_init', function () {
 			'active'   => [ 'validate_callback' => fn( $param, $request, $key ) => is_numeric( $param ) ],
 		],
 	] );
+	register_rest_route( 'bsd/v1', '/slideshows/filters', [
+		'methods'  => 'GET',
+		'callback' => 'get_slideshow_filters',
+	] );
 } );
 
 /**
@@ -47,7 +51,7 @@ function get_all_slideshows( WP_REST_Request $request ): WP_REST_Response {
 	$items = [];
 
 	foreach ( $slideshows->get_posts() as $slideshow ) {
-		$items[] = $slideshow->to_array() + get_fields( $slideshow->ID );
+		$items[] = format_slideshow( $slideshow );
 	}
 
 	$data = [
@@ -58,4 +62,45 @@ function get_all_slideshows( WP_REST_Request $request ): WP_REST_Response {
 	];
 
 	return new WP_REST_Response( compact( 'data' ) );
+}
+
+/**
+ * Lista os filtros disponiveis para serem utilizados na listagem dos veiculos
+ *
+ * @param WP_REST_Request $request
+ *
+ * @return WP_REST_Response
+ */
+function get_slideshow_filters( WP_REST_Request $request ): WP_REST_Response {
+	$data = [];
+
+	$categories = get_terms( [
+		'taxonomy'   => 'categories',
+		'hide_empty' => false,
+	] );
+
+	$data['categories'] = array_map(
+		static fn( $brand ) => [ 'id' => $brand->term_id, 'name' => $brand->name, ],
+		$categories
+	);
+
+	return new WP_REST_Response( compact( 'data' ) );
+}
+
+/**
+ * @param $slideshow
+ *
+ * @return array
+ */
+function format_slideshow( $slideshow ): array {
+	return [
+		'name'           => $slideshow->post_title,
+		'legend'         => $slideshow->post_excerpt,
+		'link'           => get_field( 'link', $slideshow->ID ),
+		'new_window'     => get_field( 'new_window', $slideshow->ID ),
+		'imagem_desktop' => get_field( 'imagem_desktop', $slideshow->ID ),
+		'imagem_mobile'  => get_field( 'imagem_mobile', $slideshow->ID ),
+		'ordem'          => get_field( 'ordem', $slideshow->ID ),
+		'status'         => get_field( 'status', $slideshow->ID ),
+	];
 }
