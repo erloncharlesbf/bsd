94facac5e62f91ab215e03df9a7cd83de6c3f272
Erlon Charles <erloncharlesbf@gmail.com>
2022-05-10 09:59:50 -0300

    Novo en dpoint de options

---
+++ wp-content/themes/bsd/custom/api/options.php
@@ -0,0 +1,33 @@
+<?php
+
+add_action( 'rest_api_init', function () {
+	register_rest_route( 'bsd/v1', '/options', [
+		'methods'  => 'GET',
+		'callback' => 'get_options',
+	] );
+} );
+
+/**
+ * Lista todos os slideshows
+ *
+ * @param WP_REST_Request $request
+ *
+ * @return WP_REST_Response
+ */
+function get_options( WP_REST_Request $request ): WP_REST_Response {
+	$options = get_option( 'bsd_options' );
+
+	$data = [
+		'name'        => get_option( 'blogname' ),
+		'description' => get_option( 'blogdescription' ),
+		'facebook'    => get_option( 'facebook_option' ),
+		'instagram'   => get_option( 'instagram_option' ),
+		'linkedin'    => get_option( 'linkedin_option' ),
+		'whatsapp'    => get_option( 'whatsapp_option' ),
+		'twitter'     => get_option( 'twitter_option' ),
+		'youtube'     => get_option( 'youtube_option' ),
+		'analytics'   => get_option( 'analytics_option' )
+	];
+
+	return new WP_REST_Response( compact( 'data' ) );
+}
