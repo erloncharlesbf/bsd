6784be8744a87e33c19bf01e05781bd2956be9bb
Erlon Charles <erloncharlesbf@gmail.com>
2022-05-13 17:30:24 -0300

    ordenando slides pelo order

--- wp-content/themes/bsd/custom/api/slideshow.php
+++ wp-content/themes/bsd/custom/api/slideshow.php
@@ -31,8 +31,8 @@ function get_all_slideshows( WP_REST_Request $request ): WP_REST_Response {
 		'post_type'      => 'slideshows',
 		'posts_per_page' => $per_page,
 		'post_status'    => 'publish',
-		'meta_key'       => 'ordem',
-		'orderby'        => 'meta_value',
+//		'meta_key'       => 'ordem',
+//		'orderby'        => 'meta_value',
 		'paged'          => $paged,
 	];
 
